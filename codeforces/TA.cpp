a8ea51b23fed8458efc6ec6c8992c95c5c8cc44c
yuriYanZexuan <1364580884@qq.com>
2022-10-01 11:06:09 +0800

    exchange MinGw

---
+++ codeforces/TA.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=1000000+10;
+deque<int> dq1,dq2;
+char str[maxn];
+int main()
+{
+    cout<<"hi"<<endl;
+    // int t;
+    // cin>>t;
+    // while(t--){
+    //     // string s;
+    //     // stringstream ss,ss2;
+    //     int n,type,c;
+    //     cin>>n;
+    //     cout<<"hellow"<<endl;
+    //     // getline(cin,s);
+    //     // getline(cin,s);
+    //     // for(int i=0;i<n;i++){
+    //     //     ss>>type;
+    //     //     ss2>>c;
+    //     //     if(type){
+    //     //         dq2.push_back(c);
+    //     //     }
+    //     //     else{
+    //     //         dq1.push_back(c);
+    //     //     }
+    //     // }
+
+    // }
+    return 0;
+}
